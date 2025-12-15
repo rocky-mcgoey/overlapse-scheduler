06d3166bf27bb0285d4d371a1e031a166ba4c0ab
JulesRuby <JulesRuby@users.noreply.github.com>
2025-12-14 18:41:08 -0700

    imported AsyncStorage and url-polyfill

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,15 @@
-import { createClient } from "@supabase/supabase-js";
+import 'react-native-url-polyfill/auto';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createClient } from '@supabase/supabase-js';
 
 const supabaseURL = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseURL, supabaseKey);
+export const supabase = createClient(supabaseURL, supabaseKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  }
+});
